539d908541e64b345b0faa500bbf4934867ece29
Janet-J <jkn6216@gmail.com>
2020-04-27 15:12:57 +0300

    link js and html

---
+++ main.js
@@ -0,0 +1,31 @@
+var getId='beans,peas,rice,potato';
+var storage= getId.split(",");
+
+storage.forEach(function(logOnSeparateLine){
+    console.log(logOnSeparateLine);
+}) 
+
+/*function myShopping(){
+   var secondStorage;
+   secondStorage=storage.slice();
+   return secondStorage;
+   
+}
+console.log(myShopping());
+// myShopping().forEach(inputitems => {
+//     console.log(inputitems)
+    
+// });
+
+//function myFunction(){
+ 
+//var inputString
+*/
+
+var inputString="how are you";
+var secondString=Array.prototype.secondString
+var newString=secondString.call(eachLetter=> {
+return `${eachLetter}a`
+})
+console.log(newString);
+
